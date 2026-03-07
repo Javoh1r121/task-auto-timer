4e0f0816779bf6c9511b0c777a132437eac41846
Javohir <java2006007@gmail.com>
2026-03-07 17:28:33 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@ Task Auto Timer automatically tracks time spent on project tasks based on stage 
 A dedicated CTO Test Timer records testing time and labor cost separately.
 Full reporting, CRM integration, and all settings configurable without code.
     """,
-    "author": "Javahir Odoo developer ",
+    "author": "Javahir",
     "website": "https://github.com/Javoh1r121",
     "license": "LGPL-3",
     "price": 0,
